d80d3002ff67e94be526a44bf16fc998718fa27b
Jonas <jonasalbuquerque@alu.ufc.br>
2019-07-03 22:09:37 -0300

    Correções

--- celulaiot/settings.py
+++ celulaiot/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'q9f)!l+($u@$_86g51_s1!uo&w9m3=v@zyiu09l8zd%=i!sxww'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['celulaiot.herokuapp.com']
 
