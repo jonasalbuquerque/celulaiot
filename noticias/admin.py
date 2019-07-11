20c29ab59ff012f5192ddf89d899fb157734b721
Jonas <jonasalbuquerque@alu.ufc.br>
2019-07-11 16:20:35 -0300

    atualizações

--- noticias/admin.py
+++ noticias/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Noticia
 
-# Register your models here.
+
+class NoticiaAdmin(admin.ModelAdmin):
+    list_display = ('nome')
+   
+admin.site.register(Noticia)
