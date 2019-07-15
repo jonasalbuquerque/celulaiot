0bfde79939c350d82cc5489e19ecad4ed0dae28c
Jonas Albuquerque <jonasalbuquerque@alu.ufc.br>
2019-07-15 12:23:56 -0300

    Deploy Statics

--- celulaiot/settings.py
+++ celulaiot/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['celulaiot.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,8 +44,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
