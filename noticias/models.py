036006a23eb46ea69a03098d0082b4ddeadeedd8
Jonas Albuquerque <jonasalbuquerque@alu.ufc.br>
2019-08-16 01:16:21 -0300

    Tudo funcionando com as novas cores. Comecei a implementar o forms tbm

--- noticias/models.py
+++ noticias/models.py
@@ -2,3 +2,8 @@ from django.db import models
 
 class Noticia(models.Model):
     ativo = models.BooleanField()
+
+class Message(models.Model):
+    name = models.CharField(max_length=100)
+    message = models.TextField()
+    email = models.EmailField()
--- noticias/views.py
+++ noticias/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .forms import MessageForm
+
 
 def index(request):
     return render(request, 'noticias/index.html', {})
